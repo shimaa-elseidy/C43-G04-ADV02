84c21b2ab4db9e4bec011f198a437eda0cc2a00f
Shimaa Elseidy <shimaaelseidy123@gmail.com>
2025-01-28 01:31:24 +0200

    01 - What is DS

--- Demo Of C# Advanced/Program.cs
+++ Demo Of C# Advanced/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region What is DS
+            // Collection     :: built in DS In c#
+            // Array          :: container has data
+            // Data Structure :: has array based & node based
+            // array based    :: like array every element has index  ====> Linear DS
+            // node based     :: value itself & next elemnt (Address)===> Non-Linear DS || Linear DS
+            // Hash Tables    :: DS of type Array Based el elemnt has [Key - Value] ==> key unique
+            // Hash Tables    :: help us to deal with big amount of data and complex
+            #endregion
         }
     }
 }
