82fa4305a4eb8fe5a74301384c53be3e7b837301
Shimaa Elseidy <shimaaelseidy123@gmail.com>
2025-01-28 01:25:08 +0200

    Add project files.

---
+++ Demo Of C# Advanced/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Demo_Of_C__Advanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
